a097d61ebdb2454885a4554f1d9c244e6172f1a1
Spencer LaValle <lavalles@lafayette.edu>
2017-02-13 14:26:29 -0500

    Added alt-constructor for network layer and cleaned up some code in the try/catch blocks

--- NetworkLayer.java
+++ NetworkLayer.java
@@ -1,13 +1,18 @@
 import java.util.concurrent.TimeUnit;
 public class NetworkLayer
 {
-
+    long delay;
     private LinkLayer linkLayer;
 
     public NetworkLayer(boolean server)
     {
         linkLayer = new LinkLayer(server);
 
+    }
+    public NetworkLayer(boolean server, long delay)
+    {
+        linkLayer = new LinkLayer(server);
+        this.delay = delay;
     }
     public void send(byte[] payload)
     {
@@ -22,15 +27,22 @@ public class NetworkLayer
     private void propDelay(long delay){
         //add consistent delay here...
         try{
-        TimeUnit.MILLISECONDS.sleep(delay);
-    }catch(InterruptedException e){}
+            TimeUnit.MILLISECONDS.sleep(delay);
+        }
+        catch(InterruptedException e){
+            System.out.println(e.stackTrace());
+        }
     }
     private void networkDelay(byte[] payload){
         int sizePayload = (payload.length)-1;
-        long delay = 0;
+        long timeDelay = 1000; //1000ms
+        long delayDuration = (sizePayload) / (timeDelay);
         //add delay based on payload length/size via sizePayload...
         try{
-        TimeUnit.MILLISECONDS.sleep(delay);
-    }catch(InterruptedException e){}
+            TimeUnit.MILLISECONDS.sleep(timeDelay);
+        }
+        catch(InterruptedException e){
+            System.out.println(e.stackTrace());
+        }
     }
 }
