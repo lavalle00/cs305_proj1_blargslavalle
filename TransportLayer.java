e6c1102c34b0f3b9fbe6e8b804e5afdaa3f54fbf
Spencer LaValle <lavalles@lafayette.edu>
2017-02-18 14:45:13 -0500

    2ndary const. again...

--- TransportLayer.java
+++ TransportLayer.java
@@ -6,9 +6,18 @@ public class TransportLayer
     byte[] arr_syn, arr_ack;
     byte[] masterPayload;
     private NetworkLayer networkLayer;
+    
     //server is true if the application is a server (should listen) or false if it is a client (should try and connect)
     public TransportLayer(boolean server){
         networkLayer = new NetworkLayer(server);
+        this.str_syn = "syn";
+        this.str_ack = "ack";
+        this.arr_syn = str_syn.getBytes();
+        this.arr_ack = str_ack.getBytes();
+    }
+    
+    public TransportLayer(boolean server, long delay){
+        networkLayer = new NetworkLayer(server, delay);
         this.str_syn = "syn";
         this.str_ack = "ack";
         this.arr_syn = str_syn.getBytes();
@@ -18,13 +27,15 @@ public class TransportLayer
     public void send(byte[] payload){
         if(payload == this.arr_ack || payload == this.arr_syn ){
             
-        }else{
-         this.masterPayload = payload;
+        }
+        else{
+            this.masterPayload = payload;
         }
         if(payload == this.arr_ack){
             
-        }else{
-         threeWay(str_syn);
+        }
+        else{
+            threeWay(str_syn);
         }
        
         System.out.println("Transport\t\tSend");
@@ -33,6 +44,7 @@ public class TransportLayer
 
     public byte[] receive(){
         byte[] tempReceive = networkLayer.receive();
+        
         //recieve syn
         if(tempReceive == this.arr_syn){
             //respond w/ ack
